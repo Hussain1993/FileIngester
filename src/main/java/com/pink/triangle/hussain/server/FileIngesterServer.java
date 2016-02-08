eeded798c11fafc5d498f37874277fd351baf6c1
Hussain Miah <hmiah.1993@gmail.com>
2016-02-08 12:48:56 +0000

    Added the file watcher to when the server starts up.

--- src/main/java/com/pink/triangle/hussain/server/FileIngesterServer.java
+++ src/main/java/com/pink/triangle/hussain/server/FileIngesterServer.java
@@ -1,13 +1,16 @@
 package com.pink.triangle.hussain.server;
 
+import com.pink.triangle.hussain.config.ApplicationConfig;
 import com.pink.triangle.hussain.config.ConfigManager;
 import com.pink.triangle.hussain.elastic.client.ElasticClient;
+import com.pink.triangle.hussain.file.FileWatcher;
 import io.searchbox.client.JestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.File;
 
 /**
  * Created by Hussain on 08/02/2016.
@@ -22,6 +25,10 @@ public class FileIngesterServer implements ServletContextListener {
         ConfigManager.init();
         client = ElasticClient.getInstance();
         LOG.info("The client has started");
+        ApplicationConfig applicationConfig = ConfigManager.getApplicationConfig();
+        File landingFolder = new File(applicationConfig.getLandingDir());
+        Thread fileWatcherThread = new Thread(new FileWatcher(landingFolder.toPath()));
+        fileWatcherThread.start();
     }
 
     public void contextDestroyed(ServletContextEvent sce) {

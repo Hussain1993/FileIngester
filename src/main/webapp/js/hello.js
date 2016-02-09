101ae62159a597553c9cf0ee06639b5d1fab10d6
Hussain Miah <hmiah.1993@gmail.com>
2016-02-09 13:04:05 +0000

    Added Bootstrap and have added the last synch date to the homepage

--- src/main/webapp/js/hello.js
+++ src/main/webapp/js/hello.js
@@ -4,3 +4,10 @@ function Hello($scope, $http) {
             $scope.greeting = data;
         });
 }
+
+function SynchStatus($scope, $http){
+    $http.get('http://localhost:8080/FileIngester/fi/tasking/synchStatus').
+        success(function(data){
+            $scope.synchStatus = data;
+        });
+}

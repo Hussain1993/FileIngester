c8e8fca07733081f9df0057b3052845284e60d96
Hussain Miah <hmiah.1993@gmail.com>
2016-02-09 12:28:28 +0000

    Added a constructor to the class

--- src/main/java/com/pink/triangle/hussain/rest/Tasking.java
+++ src/main/java/com/pink/triangle/hussain/rest/Tasking.java
@@ -1,5 +1,7 @@
 package com.pink.triangle.hussain.rest;
 
+import com.pink.triangle.hussain.config.ApplicationConfig;
+import com.pink.triangle.hussain.config.ConfigManager;
 import com.pink.triangle.hussain.elastic.client.ElasticClient;
 import com.pink.triangle.hussain.elastic.model.SynchStatus;
 
@@ -13,13 +15,24 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/tasking")
 public class Tasking {
+    private String synchStatusIndexName;
+    private String synchStatusType;
+    private String synchStatusId;
+
+    public Tasking(){
+        ApplicationConfig applicationConfig = ConfigManager.getApplicationConfig();
+
+        synchStatusIndexName = applicationConfig.getSynchStatusIndexName();
+        synchStatusType = applicationConfig.getSynchStatusType();
+        synchStatusId = applicationConfig.getSynchStatusId();
+    }
 
     @GET
     @Path("/synchStatus")
     @Produces(MediaType.APPLICATION_JSON)
     public SynchStatus getSynchStatus(){
-        SynchStatus synchStatus = (SynchStatus) ElasticClient.getItem("synch_status","synch-status","synch-id",
-                SynchStatus.class);
+        SynchStatus synchStatus = (SynchStatus) ElasticClient.getItem(synchStatusIndexName,synchStatusType,
+                synchStatusId, SynchStatus.class);
         return synchStatus;
     }
 }
